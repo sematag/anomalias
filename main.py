4b7eadf5d6ce2a9a41fc1775ab980f6b5119b870
Sergio Martinez <sematag@gmail.com>
2022-11-11 13:09:50 -0300

    API fixs

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from anomalias.detectors import Detectors
 import anomalias.api as api
+import threading
 
 
 def main():
+    threading.current_thread().name = "anomalias"
     detectors = Detectors()
     api.init(detectors)
 
