8a37ffb9c736e80cda9908fe63ee40108cd9973d
Gastón García González <68783507+GastonGarciaGonzalez@users.noreply.github.com>
2023-04-19 10:12:11 -0300

    primer_commit

---
+++ anomalias/dc-vaemodel.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 29 07:03:05 2022
+
+@author: gastong@fing.edu.uy
+"""
+
+
+
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+from tensorflow.keras.models import Model
+from tensorflow.keras import backend as K
+from tensorflow import keras
+import pandas as pd
+import numpy as np
+import pickle
+from utils import preprocessing, MTS2UTS_cond
+from sklearn.preprocessing import StandardScaler
+
+from anomalias import log
+logger = log.logger('dcvae')
+
+
+@keras.utils.register_keras_serializable()
+class Sampling(Layer):
+    """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
+    def __init__(self, name=None, k=1, **kwargs):
+        super(Sampling, self).__init__(name=name)
+        self.k = k
+        super(Sampling, self).__init__(**kwargs)
+        
+    def get_config(self):
+        config = super(Sampling, self).get_config()
+        config['k'] = self.k
+        return config #dict(list(config.items()))
+    
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        batch = K.shape(z_mean)[0]
+        seq = K.shape(z_mean)[1]
+        dim = K.shape(z_mean)[2]
+        epsilon = K.random_normal(shape=(batch, seq, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+
+class DCVAE:
+    def __init__(self, th_sigma, th_lower=None, th_upper=None, 
+                 model_path='', 
+                 model_name='dc-vae_best_model',
+                 T=128,
+                 batch_size=32,
+                 epochs=100,
+                   **kwargs):
+        logger.info('Setting DCVAE model.')
+        self.__th_sigma = th_sigma
+        self.__th_lower = th_lower
+        self.__th_upper = th_upper
+
+        self.__T = T
+        self.__batch_size = batch_size
+        self.__epochs = epochs
+        self.__scaler = StandardScaler()
+        self.__model_name = model_name
+
+        logger.info('Model name: '+model_name)
+        self.__model = None
+        self.__init = True
+        self.__trained = True
+        self.__model_fit = keras.models.load_model(model_path+model_name+'.h5',
+                                                    custom_objects={'sampling': Sampling},
+                                                    compile = True)
+
+    def fit(self, df=None, val_percent=0.1):
+    
+        # Data preprocess
+        df_process = preprocessing(df, flag_scaler=True, scaler=self.__scaler, 
+                        scaler_name=self.__model_name, outliers_out=True, outliers_max_std=7,
+                        instance='fit')
+
+        X, cond_info = MTS2UTS_cond(df_process, T=self.__T)
+        ix_rand = np.random.permutation(X.shape[0])
+        X = np.array(X)[ix_rand]
+        cond_info = np.array(cond_info)[ix_rand]  
+
+        # Callbacks
+        early_stopping_cb = keras.callbacks.EarlyStopping(min_delta=1e-2,
+                                                      patience=5,                                            
+                                                      verbose=1,
+                                                      mode='min')
+        model_checkpoint_cb= keras.callbacks.ModelCheckpoint(
+            filepath=self.name+'_best_model.h5',
+            verbose=1,
+            mode='min',
+            save_best_only=True)
+        
+          
+        # Model train
+        self.__history_ = self.__model_fit.fit((X, cond_info),
+                     batch_size=self.batch_size,
+                     epochs=self.epochs,
+                     validation_split = val_percent,
+                     callbacks=[early_stopping_cb,
+                                model_checkpoint_cb]
+                     ) 
+        
+        # Save models
+
+        self.__model_fit.vae.save(self.__model_name+'_refit.h5')
+
+        return self
+         
+    def detect(self, df):
+        if self.__init:
+            self.__model_fit = self.__model_fit.apply(endog=self.__pre(df), refit=False)
+            self.__init = False
+
+
+        # Inference model. Auxiliary model so that in the inference 
+        # the prediction is only the last value of the sequence
+        inp = Input(shape=(self.T, self.M))
+        x = self.vae(inp) # apply trained model on the input
+        out = Lambda(lambda y: [y[0][:,-1,:], y[1][:,-1,:]])(x)
+        inference_model = Model(inp, out)
+        
+        # Data preprocess
+        sam_val, sam_ix, sam_class = MTS2UTS_cond(df_X, T=self.T)
+        
+        # Predictions
+        prediction = self.vae.predict(np.stack(sam_val))
+        # The first T-1 data of each sequence are discarded
+        reconstruct = prediction[0]
+        sig = np.sqrt(np.exp(prediction[1]))
+        
+        # Data evaluate (The first T-1 data are discarded)
+        df_evaluate = UTS2MTS(sam_val, sam_ix, sam_class)
+        df_reconstruct = UTS2MTS(reconstruct, sam_ix, sam_class)
+        df_sig = UTS2MTS(sig, sam_ix, sam_class)
+        
+        # Thresholds
+        if len(alpha_set) == self.M:
+            alpha = np.array(alpha_set)
+        elif load_alpha:
+            with open(self.name + '_alpha.pkl', 'rb') as f:
+                alpha = pickle.load(f)
+                f.close()
+        else:
+            alpha = self.alpha
+            
+        thdown = df_reconstruct.values - alpha*df_sig.values
+        thup = df_reconstruct.values + alpha*df_sig.values
+        
+        # Evaluation
+        pred = (df_evaluate.values < thdown) | (df_evaluate.values > thup)
+        df_predict = pd.DataFrame(pred, columns=df_X.columns, index=df_X.iloc[self.T-1:].index)
+        
+        if only_predict:
+            return df_predict
+        else:
+            latent_space = self.encoder.predict(np.stack(sam_val))[2]
+            return df_predict, df_reconstruct, df_sig, latent_space, sam_ix, sam_class
+
+
+    def evaluate(self, df_X=None, load_model=False, model='best_model'):
+        # Data preprocess
+        sam_val, sam_ix, sam_class = MTS2UTS(df_X, T=self.T)
+
+        # Trained model
+        if load_model:
+            print('=====================================')
+            self.vae = keras.models.load_model(self.name+'_'+model+'.h5',
+                                                    custom_objects={'sampling': Sampling},
+                                                    compile = True)
+
+        # Model evaluate
+        value_elbo, reconstruction, kl = self.vae.evaluate(np.stack(sam_val), np.stack(sam_val),
+                     batch_size=self.batch_size,
+                     )  
+        
+        return value_elbo, reconstruction, kl
+        
---
+++ anomalias/utils.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 29 14:34:43 2022
+
+@author: gastong@fing.edu.uy
+"""
+
+
+import pandas as pd
+import numpy as np
+import pickle
+
+
+def set_index(dataRaw):
+    col_time = dataRaw.columns[0]
+    dataRaw[col_time] = pd.to_datetime(dataRaw[col_time])
+    dataRaw = dataRaw.set_index(col_time)
+    return dataRaw
+
+
+def preprocessing(dataRaw=None, flag_scaler=True, scaler=None, scaler_name=None,
+                  outliers_out=False, outliers_max_std=7, instance='fit'):
+    
+    X = dataRaw.copy()
+    
+    if outliers_out:
+        z_scores = (X-X.mean())/X.std()
+        abs_z_scores = np.abs(z_scores)
+        filtered_entries = (abs_z_scores > outliers_max_std)
+        X = X.mask(filtered_entries)
+
+    X.fillna(method='ffill', inplace=True) 
+    X.fillna(method='bfill', inplace=True)
+
+    if flag_scaler:
+        if instance == 'fit':
+            X = scaler.fit_transform(X)
+            pickle.dump(scaler, open(scaler_name + '_scaler.pkl','wb'))
+        elif instance == 'transform':
+            scaler = pickle.load(open(scaler_name + '_scaler.pkl','rb'))
+            X = scaler.transform(X)
+        elif instance == 'inverse':
+            scaler = pickle.load(open(scaler_name + '_scaler.pkl','rb'))
+            X = scaler.inverse_transform(X)
+    
+    X = pd.DataFrame(X, index=dataRaw.index, columns=dataRaw.columns)
+    return X     
+
+
+def sincos_transf(x, period):
+    return np.stack((np.sin(x / period * 2 * np.pi), np.cos(x / period * 2 * np.pi))).T  
+
+def time_info_embedd(ds=None):
+    
+    index = ds.index
+
+    days = index.day
+    weekdays = index.weekday
+    hours = index.hour
+    minutes = index.minute
+
+    days_embedd = sincos_transf(days, period=31)
+    weekdays_embedd = sincos_transf(weekdays, period=7)
+    hours_embedd = sincos_transf(hours, period=24)
+    minutes_embedd = sincos_transf(minutes, period=60)
+
+    return np.concatenate([days_embedd, weekdays_embedd, hours_embedd, minutes_embedd], axis=-1)
+
+
+def samples_conditions_embedd(ds=None, T=32):
+    N, _ = ds.shape
+    values = ds.values
+    time_cond = time_info_embedd(ds)
+
+    samples = [values[i: i + T] for i in range(0, N - T+1)]  
+    samples_time_embedd = [time_cond[i: i + T] for i in range(0, N - T+1)]
+    
+    return samples, samples_time_embedd
+
+def MTS2UTS_cond(ds=None, T=32, seed=42):
+    N, C = ds.shape
+    columns = ds.columns
+    col_embedd = np.arange(C)/C
+    time_cond = time_info_embedd(ds)
+    samples_aux_time_cond = [time_cond[i: i + T] for i in range(0, N - T+1)]
+    
+    samples_time_cond = []
+    samples_values = []
+    samples_class = []
+    for c in range(C):
+        serie_values = ds.iloc[:,c].values
+        samples_aux_values = [serie_values[i: i + T] for i in range(0, N - T+1)]  
+        samples_aux_class = list(col_embedd[c]*np.ones((len(samples_aux_values), T, 1)))
+        samples_time_cond += samples_aux_time_cond
+        samples_values += samples_aux_values
+        samples_class += samples_aux_class
+
+    samples_values = np.array(samples_values)
+    samples_info = np.concatenate([np.array(samples_time_cond), np.array(samples_class)], axis=-1) 
+    return samples_values, samples_info
