c34130faafb8f9027a72f1cda34a91f75adebe7a
Gastón Darío García González <g.garciagonzalez50@gmail.com>
2023-08-02 11:31:43 -0300

    debugg

--- anomalias/api.py
+++ anomalias/api.py
@@ -285,7 +285,7 @@ def init(detectors):
             df = df.asfreq(data.freq)
 
             logger.debug('api.py: call to detect(), data:')
-            logger.debug('\n %s', df)
+            logger.debug('\n %s', df.isnull().sum())
 
             detectors.append(df_id, df)  # Detection
         except Exception as e:
